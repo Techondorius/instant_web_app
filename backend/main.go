53b847a957e5d741d1cddbe4945ced0fe5b2b320
Kyosuke Fujita <kurofuku01@gmail.com>
2023-06-29 20:12:04 +0900

    devcontainerで初期化した

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "fmt"
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "secret_message",
-		})
-	})
-	r.Run(":8080")
+	fmt.Println("Hello!")
 }
